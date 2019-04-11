4ecdba88c3d0b0c7b22f913d49f51b194c75edca
Brijesh Kumar Gupta <eltronicsvilla17@gmail.com>
2019-04-11 16:24:17 +0530

    duplicated codes are deleted

--- draw_ecllipse.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2
-
-img = np.zeros((500,500,3),np.uint8)
-cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
-cv2.imshow('output',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
